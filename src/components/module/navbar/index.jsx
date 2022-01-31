33d14b23dbf782968befc845c1f2753b23306b25
herzaparam <herzaparam@gmail.com>
2022-02-01 00:16:34 +0700

    maintain view all film page, search navbar, pagination

--- src/components/module/navbar/index.jsx
+++ src/components/module/navbar/index.jsx
@@ -1,145 +1,213 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
-import { Link, useHistory } from 'react-router-dom'
-import { getUser } from '../../../configs/redux/actions/user'
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, useHistory } from 'react-router-dom';
+import { getUser } from '../../../configs/redux/actions/user';
 
-import MyButton from '../../base/Button'
-import { tickitzpurpleimg, searchlogo } from '../../../assets/image'
-import style from './navbar.module.css'
-import axios from 'axios'
-import Swal from 'sweetalert2'
+import MyButton from '../../base/Button';
+import { tickitzpurpleimg, searchlogo } from '../../../assets/image';
+import style from './navbar.module.css';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 function MyNavbar({ isLoggedIn }) {
-    let history = useHistory();
-    const dispatch = useDispatch();
-    const urlImg = process.env.REACT_APP_API_IMG
+  let history = useHistory();
+  const dispatch = useDispatch();
+  const urlImg = process.env.REACT_APP_API_IMG;
 
-    const [searchTerm, setSearchTerm] = useState('');
-    const [showSearch, setShowSearch] = useState(false);
-    const [authLogin, setAuthLogin] = useState(isLoggedIn)
-    const [allFilms, setAllfilms] = useState([])
+  const [searchTerm, setSearchTerm] = useState('');
+  const [showSearch, setShowSearch] = useState(false);
+  const [authLogin, setAuthLogin] = useState(isLoggedIn);
+  const [allFilms, setAllfilms] = useState([]);
 
-    const { user,role } = useSelector(state => state.userReducer)
-    // const { allFilms } = useSelector(state => state.movieReducer)
+  const { user, role } = useSelector((state) => state.userReducer);
+  // const { allFilms } = useSelector(state => state.movieReducer)
 
-    const handleShowSearch = (e) => {
-        e.preventDefault();
-        if (showSearch === true) {
-            return setShowSearch(false)
-        }
-        setShowSearch(true)
+  const handleShowSearch = (e) => {
+    e.preventDefault();
+    setAllfilms([])
+    setShowSearch(!showSearch);
+  };
+
+  const handleLogOut = (e) => {
+    localStorage.removeItem('token');
+    setAuthLogin(false);
+    dispatch({ type: 'LOG_OUT' });
+    history.push('/');
+  };
+
+  useEffect(() => {
+    if (user.email === '') {
+      dispatch(getUser());
     }
-
-    const handleLogOut = (e) => {
-        localStorage.removeItem("token")
-        setAuthLogin(false)
-        dispatch({type: 'LOG_OUT'})
-        history.push("/")
+  }, []);
+  useEffect(() => {
+    if (searchTerm !== '') {
+      axios
+        .get(
+          `${process.env.REACT_APP_SEARCH_MOVIE_FREE_API}?api_key=${process.env.REACT_APP_API_KEY}&query=${searchTerm}`
+        )
+        .then((res) => {
+          setAllfilms(res.data.results);
+        })
+        .catch((err) => {
+          if (err.response.status === 404) {
+            return allFilms;
+          }
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Something went wrong!',
+            timer: 1000,
+            showConfirmButton: false,
+          });
+        });
     }
+  }, [searchTerm]);
 
-    useEffect(() => {
-        if (user.email === "") {
-            dispatch(getUser())
-        }
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img src={tickitzpurpleimg} alt="" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/allmovies"
+              >
+                Movies
+              </Link>
+            </li>
+            <li className={[['nav-item'], style['disabled']].join(' ')}>
+              <button disabled={true}>Cinemas</button>
+            </li>
+            <li className={[['nav-item'], style['disabled']].join(' ')}>
+              <button disabled={true}>Buy Ticket</button>
+            </li>
+          </ul>
+          <div className="nav-item dropdown">
+            <button className={style['btn-loc-disable']} disabled={true}>
+              Location
+            </button>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <Link className="dropdown-item" to="#">
+                  Jakarta
+                </Link>
+              </li>
+              <li>
+                <Link className="dropdown-item" to="#">
+                  Surabaya
+                </Link>
+              </li>
+            </ul>
+          </div>
 
-    }, [])
-    useEffect(() => {
-        if (searchTerm !== "") {
-            axios.get(`${process.env.REACT_APP_API_TICKITZ}movies/all-movies/?page=1&perPage=5&keyword=${searchTerm}`)
-                .then((res) => {
-                    setAllfilms(res.data.data)
-                })
-                .catch((err) => {
-                    if (err.response.status === 404) {
-                        return allFilms
-                    }
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: 'Something went wrong!',
-                        timer: 1000,
-                        showConfirmButton: false
-                    })
-                })
-        }
-
-    }, [searchTerm])
-
-    return (
-        <nav className="navbar navbar-expand-lg navbar-light">
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/"><img src={tickitzpurpleimg} alt="" /></Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <Link className="nav-link active" aria-current="page" to="/allmovies">Movies</Link>
-                        </li>
-                        <li className={[["nav-item"], style["disabled"]].join(' ')} >
-                            <button disabled={true}>Cinemas</button>
-                        </li>
-                        <li className={[["nav-item"], style["disabled"]].join(' ')}>
-                            <button disabled={true}>Buy Ticket</button>
-                        </li>
-
-                    </ul>
-                    <div className="nav-item dropdown">
-                      <button className={style["btn-loc-disable"]} disabled={true}>Location</button>
-                        <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <li><Link className="dropdown-item" to="#">Jakarta</Link></li>
-                            <li><Link className="dropdown-item" to="#">Surabaya</Link></li>
-                        </ul>
-                    </div>
-
-                    <form className={`d-flex `}>
-                        {showSearch ?
-                            <div className={style.dropdown}>
-                                <div className={style.search}>
-                                    <input className={`dropdown-header form-control me-2 ${style.inputSearch}`} type="search" placeholder="Search" aria-label="Search" onChange={e => { setSearchTerm(e.target.value) }} />
-                                    <button className="btn btn-outline-light" type="submit" onClick={handleShowSearch}><img src={searchlogo} alt="" /></button>
-                                </div>
-                                <div className={style.dropdownContent}>
-                                    {allFilms.filter((film) => {
-                                        if (searchTerm === "") {
-                                            return film
-                                        } else if (film.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-                                            return film
-                                        }
-                                    }).map((film, key) => {
-                                        return (<div className={`${style.film}`} key={key}>
-                                            <img src={film.image} alt="" />
-                                            <p>{film.title}</p>
-                                        </div>)
-                                    })}
-                                </div>
-                            </div> :
-                            <button className="btn btn-outline-light" type="submit" onClick={handleShowSearch}><img src={searchlogo} alt="" /></button>
-                        }
-                    </form>
-                    <div className="nav-item dropdown">
-                        {user.email === "" ?
-                            <MyButton className="btn-order" title="Sign Up" onClick={(e) => history.push("/signup")} /> :
-                            <div>
-                                <button className={`nav-link ${style.btnProfile}`} to="#" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                                    <img src={`${urlImg}${user.image}`} alt="" />
-                                </button>
-                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><button className="dropdown-item" onClick={() => { history.push("/profile") }}>Profile</button></li>
-                                    <li><button className="dropdown-item" to="#" onClick={handleLogOut}>Logout</button></li>
-                                </ul>
-                            </div>
-                        }
-                    </div>
-
-
-
+          <form className={`d-flex `}>
+            {showSearch ? (
+              <div className={style.dropdown}>
+                <div className={style.search}>
+                  <input
+                    className={`dropdown-header form-control me-2 ${style.inputSearch}`}
+                    type="search"
+                    placeholder="Search"
+                    aria-label="Search"
+                    onChange={(e) => {
+                      setSearchTerm(e.target.value);
+                    }}
+                  />
+                  <button
+                    className="btn btn-outline-light"
+                    type="submit"
+                    onClick={handleShowSearch}
+                  >
+                    <img src={searchlogo} alt="" />
+                  </button>
                 </div>
-            </div>
-        </nav>
-
-    )
+                <div className={style.dropdownContent}>
+                  {allFilms?.slice(0, 6).map((film, key) => {
+                    return (
+                      <div className={`${style.film}`} key={key}>
+                        <img
+                          src={`${process.env.REACT_APP_BASE_URL_IMAGE}${film.poster_path}`}
+                          alt=""
+                        />
+                        <p>{film.original_title ?? ''}</p>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            ) : (
+              <button
+                className="btn btn-outline-light"
+                type="submit"
+                onClick={handleShowSearch}
+              >
+                <img src={searchlogo} alt="" />
+              </button>
+            )}
+          </form>
+          <div className="nav-item dropdown">
+            {user.email === '' ? (
+              <MyButton
+                className="btn-order"
+                title="Sign Up"
+                onClick={(e) => history.push('/signup')}
+              />
+            ) : (
+              <div>
+                <button
+                  className={`nav-link ${style.btnProfile}`}
+                  to="#"
+                  id="navbarDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img src={`${urlImg}${user.image}`} alt="" />
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li>
+                    <button
+                      className="dropdown-item"
+                      onClick={() => {
+                        history.push('/profile');
+                      }}
+                    >
+                      Profile
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      className="dropdown-item"
+                      to="#"
+                      onClick={handleLogOut}
+                    >
+                      Logout
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default MyNavbar
+export default MyNavbar;
--- src/pages/ViewFilm/index.jsx
+++ src/pages/ViewFilm/index.jsx
@@ -1,113 +1,132 @@
-import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import { getUser } from '../../configs/redux/actions/user'
-import MyNavbar from '../../components/module/navbar'
-import style from './view.module.css'
-import LoadingAnimation from '../../components/module/LoadingAnimation'
-import Pagination from '../../components/module/Pagination'
-import axios from 'axios'
-import Swal from 'sweetalert2'
+import React, { useState, useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
+import { getUser } from '../../configs/redux/actions/user';
+import MyNavbar from '../../components/module/navbar';
+import style from './view.module.css';
+import LoadingAnimation from '../../components/module/LoadingAnimation';
+import Pagination from '../../components/module/Pagination';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 function ViewFilm() {
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    // const { allFilms } = useSelector(state => state.movieReducer)
-    const { user } = useSelector(state => state.userReducer)
-    const { loading } = useSelector(state => state.movieReducer)
+  // const { allFilms } = useSelector(state => state.movieReducer)
+  const { user } = useSelector((state) => state.userReducer);
+  const { loading } = useSelector((state) => state.movieReducer);
 
-    const [currentPage, setCurrentPage] = useState(1)
-    const [postPerPage, setPostPerPage] = useState(8)
-    const [allFilms, setAllFilms] = useState([])
-    const [category, setCategory] = useState("")
-    const [genre, setGenre] = useState("")
+  const [currentPage, setCurrentPage] = useState(1);
+  // console.log('haha', currentPage);
+  const [allFilms, setAllFilms] = useState([]);
+  const [totalFilms, setTotalFilms] = useState(0);
+  const [category, setCategory] = useState('upcoming');
 
-    useEffect(() => {
-        // dispatch(getMovie())
-        if (user.email === "") {
-            dispatch(getUser())
-        }
-        axios.get(`${process.env.REACT_APP_API_TICKITZ}movies/all-movies/?page=1&perPage=50&keyword=`)
-            .then((res) => {
-                setAllFilms(res.data.data)
-            })
-            .catch((err) => {
-                Swal.fire({
-                    icon: 'error',
-                    title: 'Oops...',
-                    text: 'Something went wrong!',
-                    timer: 1000,
-                    showConfirmButton: false
-                })
-            })
-    }, [])
-
-    useEffect(() => {
-        if (category !== "" || genre !== "") {
-            axios.get(`${process.env.REACT_APP_API_TICKITZ}movies/all-sort/?category=${category}&genre=${genre}&page=1&perPage=50`)
-                .then((res) => {
-                    setAllFilms(res.data.data)
-                })
-                .catch((err) => {
-                    Swal.fire({
-                        icon: 'error',
-                        title: 'Oops...',
-                        text: `cannot find flim with ${category} category and ${genre} genre`,
-                        
-                    })
-                })
-        }
-    }, [category, genre])
-
-    if (loading) {
-        return <LoadingAnimation />
+  useEffect(() => {
+    // dispatch(getMovie())
+    if (user.email === '') {
+      dispatch(getUser());
     }
-    const routeChangeById = (id) => {
-        history.push(`/moviedetails/${id}`)
-    }
+    axios
+      .get(
+        `${process.env.REACT_APP_GET_MOVIE_FREE_API}/${category}?api_key=${process.env.REACT_APP_API_KEY}&page=${currentPage}`
+      )
+      .then((res) => {
+        setAllFilms(res.data.results);
+        setTotalFilms(res.data.total_results);
+      })
+      .catch((err) => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Something went wrong!',
+          timer: 1000,
+          showConfirmButton: false,
+        });
+      });
+  }, [category, currentPage]);
 
-    const indexOfLastPost = currentPage * postPerPage;
-    const indexOfFirsPost = indexOfLastPost - postPerPage;
-    const currentFilm = allFilms.slice(indexOfFirsPost, indexOfLastPost);
+  //   useEffect(() => {
+  //     if (category !== '' || genre !== '') {
+  //       axios
+  //         .get(
+  //           `${process.env.REACT_APP_API_TICKITZ}movies/all-sort/?category=${category}&genre=${genre}&page=1&perPage=50`
+  //         )
+  //         .then((res) => {
+  //           setAllFilms(res.data.data);
+  //         })
+  //         .catch((err) => {
+  //           Swal.fire({
+  //             icon: 'error',
+  //             title: 'Oops...',
+  //             text: `cannot find flim with ${category} category and ${genre} genre`,
+  //           });
+  //         });
+  //     }
+  //   }, [category]);
 
-    const paginate = (pageNumber) => setCurrentPage(pageNumber)
+  if (loading) {
+    return <LoadingAnimation />;
+  }
+  const routeChangeById = (id) => {
+    history.push(`/moviedetails/${id}`);
+  };
+  // const indexOfLastPost = currentPage * postPerPage;
+  // const indexOfFirsPost = indexOfLastPost - postPerPage;
+  // const currentFilm = allFilms?.slice(indexOfFirsPost, indexOfLastPost);
 
-    return (
-        <div>
-            <MyNavbar />
-            <div className={style["sorting"]}>
-                <select className={style["inpt-category"]} name="category" id="category" onChange={e => setCategory(e.target.value)}>
-                    <option value="now">Now Showing</option>
-                    <option value="upcoming">Upcoming</option>
-                </select>
-                <select className={style["inpt-category"]} name="genre" id="genre" onChange={e => setGenre(e.target.value)}>
-                    <option value="Action">Action</option>
-                    <option value="Horror">Horror</option>
-                    <option value="Romance">Romance</option>
-                    <option value="Comedy">Comedy</option>
-                    <option value="Drama">Drama</option>
-                    <option value="Thriller">Thriller</option>
-                    <option value="Animation">Animation</option>
-                </select>
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+  return (
+    <div>
+      <MyNavbar />
+      <div className={style['sorting']}>
+        <select
+          className={style['inpt-category']}
+          name="category"
+          id="category"
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="upcoming">Upcoming</option>
+          <option value="top_rated">Top Rated</option>
+          <option value="popular">Popular</option>
+          <option value="now_playing">Now Playing</option>
+          <option value="latest">Latest</option>
+        </select>
+      </div>
+      <div
+        className={[['container'], ['bg-light'], style['card-container']].join(
+          ' '
+        )}
+      >
+        {allFilms?.map((item) => (
+          <div className={`card ${style.cardFilm}`} key={item.id}>
+            <img
+              src={`${process.env.REACT_APP_BASE_URL_IMAGE}${item.poster_path}`}
+              className="card-img-top"
+              alt="..."
+            />
+            <div className="card-body">
+              <h5 className={style['card-title']}>
+                {item.title?.length > 23
+                  ? item.title.substring(0, 23) + '...'
+                  : item.title}
+              </h5>
+              <p className={style['card-text']}>{item.genre}</p>
+              <button
+                className={style.button}
+                onClick={(e) => routeChangeById(item.movie_Id)}
+              >
+                Book Now
+              </button>
             </div>
-            <div className={[['container'], ['bg-light'], style['card-container']].join(' ')}>
-                {currentFilm.map((item) =>
-                    <div className={`card ${style.cardFilm}`} key={item.movie_Id}>
-                        <img src={item.image} className="card-img-top" alt="..." />
-                        <div className="card-body">
-                            <h5 className={style['card-title']}>{item.title.length > 13 ? item.title.substring(0, 13) + " . . . " : item.title}</h5>
-                            <p className={style['card-text']}>{item.genre}</p>
-                            <button className={style.button} onClick={(e) => routeChangeById(item.movie_Id)}>Book Now</button>
-                        </div>
-                    </div>
-                )}
-
-            </div>
-            <Pagination postPerPage={postPerPage} totalPost={allFilms.length} paginate={paginate} />
-
-        </div>
-    )
+          </div>
+        ))}
+      </div>
+      <Pagination postPerPage={20} totalPost={totalFilms} paginate={paginate} currentPage={currentPage} />
+    </div>
+  );
 }
 
-export default ViewFilm
+export default ViewFilm;
